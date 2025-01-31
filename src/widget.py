2c9eba2410f0f6cfc2e317ab718157d6ea701877
Rinat Davletyarov <agony30@yandex.ru>
2025-01-31 17:04:14 +0500

    modlule widget.get_date

--- src/widget.py
+++ src/widget.py
@@ -3,15 +3,20 @@ import masks
 
 def mask_account_card(bank_data: str) -> str:
     """ Функция для маскировки номеров счетов и карт"""
-    bank_data_number = bank_data.split()[-1]
-    beginning = bank_data[:-len(bank_data_number)]
+    bank_data_number = bank_data.split()[-1]  # Извлечение номера в формате str
+    beginning = bank_data[:-len(bank_data_number)]  # Сохранение названия кроме номера
 
-    if len(bank_data_number) == 16:
+    if len(bank_data_number) == 16:  # Если номер карты, использовать модуль для карты
         mask_number = masks.get_mask_card_number(int(bank_data_number))
-    else:
+    else:  # Раз это не карта, использовать модуль для счёта
         mask_number = masks.get_mask_account(int(bank_data_number))
 
-    return f"{beginning}{mask_number}"
+    return f"{beginning}{mask_number}"  # Возврат строки: Название + замаскированный номер
+
+
+def get_date(date_string: str) -> str:
+    """Функция для перевода даты из технического формата в читаемый"""
+    return f"{date_string[8:10]}.{date_string[5:7]}.{date_string[0:4]}"  # Возврат извлечённых данных
 
 
 # Данные для проверки
@@ -23,3 +28,5 @@ def mask_account_card(bank_data: str) -> str:
 # print(mask_account_card("Visa Platinum 8990922113665229"))
 # print(mask_account_card("Visa Gold 5999414228426353"))
 # print(mask_account_card("Счет 73654108430135874305"))
+
+print(get_date("2024-03-11T02:26:18.671407"))
