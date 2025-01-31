0c6e388e13771e66a741b74f0ad4f300ff3f6cf1
Rinat Davletyarov <agony30@yandex.ru>
2025-01-31 17:10:49 +0500

    many little bugs

--- src/widget.py
+++ src/widget.py
@@ -29,4 +29,4 @@ def get_date(date_string: str) -> str:
 # print(mask_account_card("Visa Gold 5999414228426353"))
 # print(mask_account_card("Счет 73654108430135874305"))
 
-print(get_date("2024-03-11T02:26:18.671407"))
+# print(get_date("2024-03-11T02:26:18.671407"))
