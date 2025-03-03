a1dcfa52a9415c8a6a3afe9f26b22c3271eec7c5
Rinat Davletyarov <agony30@yandex.ru>
2025-03-03 19:04:42 +0500

    create 2 files decorators and test_..

---
+++ src/decorators.py
@@ -0,0 +1,25 @@
+def log(filename=25):
+    def my_decorator(func):
+        def inner(*args, **qwargs):
+            print(f"Аргументы декоратора: {filename}")
+            result = func(*args, **qwargs)
+            return result
+        return inner
+    return my_decorator
+
+# def log(arg1=7):
+#     def my_decorator(func):
+#         def wrapper(*args, **kwargs):
+#             print(f"Аргументы декоратора: {arg1}")
+#             result = func(*args, **kwargs)
+#             print("После выполнения функции")
+#             return result
+#         return wrapper
+#     return my_decorator
+
+
+@log()
+def summerik(a, b):
+    print(a+b)
+
+summerik(1, 30)
---
+++ tests/test_decorators.py
@@ -0,0 +1,18 @@
+import pytest
+from src.decorators import log
+
+def test_log():
+    @log
+    def add_numbers(a, b):
+        return a + b
+
+    result1 = add_numbers(3, 5)
+    assert result1 == 8
+
+    @log("test_log.txt")
+    def sub_numbers(a, b):
+        return a - b
+
+    result2 = sub_numbers(21, 4)
+    assert result2 == 17
+
